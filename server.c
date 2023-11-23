c358f24edb79cce1f7915b082f3a467ed1cc6b07
Xenovitch <83357129+Xenopos@users.noreply.github.com>
2023-11-23 23:28:27 +0800

    preparation phases encountered black magic

--- server.c
+++ server.c
@@ -85,31 +85,76 @@ void clear_input_buffer() {
 void on_preparation_phase() {
     XiyaFlags xiyaFlags;
     GameState gameState;
-
     int actionsInputted = 0;
-
+    int choosingAction = 0;
     while (xiyaFlags.isPreparationPhase) {
-        int hasInvalidactioninput = 0; 
+        int hasInvalidactioninput = 0;
 
-        for (int choosingAction = 0; choosingAction < 4; choosingAction++) {
-            printf("Enter desired action for slot %d: ", choosingAction + 1);
-            if (scanf("%d", &gameState.Xiyactions[choosingAction].actioninput) != 1
-                || gameState.Xiyactions[choosingAction].actioninput < 1
-                || gameState.Xiyactions[choosingAction].actioninput > 4) {
-                hasInvalidactioninput = 1;  
-                printf("Invalid action input. Please enter numbers between 1 and 4.\n");
-                clear_input_buffer(); 
+        for (choosingAction; choosingAction < 4; choosingAction++) {
+            printf("Enter desired action for slot %d (enter 0 to clear): ", choosingAction + 1);
+            if (scanf("%d", &gameState.Xiyactions[choosingAction].actioninput) != 1) {
+                hasInvalidactioninput = 1;
+                printf("Invalid input. Please enter a number.\n");
+                clear_input_buffer();
                 break;
             }
+
+            if (gameState.Xiyactions[choosingAction].actioninput == 0) {
+                printf("Array cleared.\n");
+                hasInvalidactioninput = 1;
+                clear_input_buffer();
+                continue;
+            }
+            //imscared
+            if (gameState.Xiyactions[choosingAction].actioninput < 1 || gameState.Xiyactions[choosingAction].actioninput > 4) {
+                hasInvalidactioninput = 1;
+                printf("Invalid action input. Please enter numbers between 1 and 4 or 0 to clear.\n");
+                clear_input_buffer();
+                continue;
+            }
+
             actionsInputted++;
         }
+
         if (hasInvalidactioninput) {
             actionsInputted = 0;
+            choosingAction = 0;
             continue;
         }
-        if (actionsInputted == 4) {
-            printf("All slots are filled.\n");
+
+if (actionsInputted == 4) {
+    printf("All slots are filled.\n");
+
+    // Print the filled slots
+    for (int count = 0; count < 4; count++) {
+        int actionInput = gameState.Xiyactions[count].actioninput;
+        printf("Slot %d: %d ", count + 1, actionInput);
+
+        // Switch-case to interpret user input
+        switch (actionInput) {
+            case 1:
+                printf("Action: Attack\n");
+                // Add logic for attack
+                break;
+            case 2:
+                printf("Action: Heal\n");
+                // Add logic for heal
+                break;
+            case 3:
+                printf("Action: Defend\n");
+                // Add logic for defend
+                break;
+            case 4:
+                printf("Action: Counter\n");
+                // Add logic for counter
+                break;
+            default:
+                printf("Invalid action input.\n");
+                // Handle invalid input
+                break;
         }
+    }
+}
         break;
     }
 }
@@ -117,7 +162,8 @@ void on_preparation_phase() {
 
 void ShowIntro()
 {
-    printf("Mentee Shizuka is not ready...");
+    printf("Mentee Shizuka is not ready...\n");
+    printf("Mentee Shizuka is ready...\n");
 }
 
 
@@ -127,7 +173,7 @@ int main(int argc, char const *argv[])
 
     XiyaFlags xiyaFlags;  
     xiyaFlags.isPreparationPhase = 1; 
-
+    xiyaFlags.isExecutionPhase = 0;
     if (xiyaFlags.isPreparationPhase == 1) {
         on_preparation_phase();
     }
